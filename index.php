966e678e43dc6abf8f248c3e799e785e2e2c3ccb
Mark Larson <mark@freeform-studios.com>
2023-12-04 16:40:34 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style>
+	*
+	{
+		margin:0;
+		padding:0;
+	}
+	body
+	{
+		background:#333;
+		text-align:center;
+		font:12px/17px Arial,Helvetica,Sans-Serif;
+		color:#FC0;
+	}
+	.outside_container
+	{
+		width:700px;
+		text-align:left;
+		margin:0 auto;
+	}
+	
+	h1
+	{
+		margin-top:20px;
+		font-size:18px;
+		font-weight:normal;
+		font-family:Georgia, "Times New Roman", Times, serif;
+		padding-bottom:5px;
+		border-bottom:1px solid #c39c00;
+	}
+	
+	h2
+	{
+		font-size:14px;
+		font-family:Georgia, "Times New Roman", Times, serif;
+	}
+	
+	p
+	{
+		color:white;
+		margin:20px 0;
+	}
+	
+	.hz
+	{
+		border-top:1px solid #111;
+		border-bottom:1px solid #555;
+	}
+	
+	img
+	{
+		width:700px;
+	}
+</style>
+</head>
+
+<body>
+	<div class="outside_container">
+		<h1>Mark Larson UI Portfolio</h1>
+		<p>
+			These are hi-fi prototypes for a software project currently under development. Due to the sensitive nature of this information, the images should not be copied, distributed or otherwise used other than for display on this site.
+		</p>
+		<h2>Software Screens</h2>
+		<p>
+			<img src="_images/IP_Landing_CE_Chart_Tile.png" alt="" /><br />
+			Landing page for all interviews done for the client. The goal was to give them quick understanding of the outcome of the deals for which they were interviewed as well
+			as an overall view of the company's sales efforts.
+		</p>
+		<div class="hz"></div>
+		<p>
+			<img src="_images/IP_Profile_CE_Defector_Slideout_KPI.png" alt="" /><br />
+			One view of an individual deal profile. The slideout area is mostly for quantitiatve information while the main panel contains the interview content
+		</p>
+		<div class="hz"></div>
+		<p>
+			<img src="_images/HZ_Research_Tile.png" alt="" /><br />
+			This is the main landing page for a client's research program. The owner requires a lot of detail to be available for this screen. This caused quite a challenge
+			to display his desired information while making the interface intuitive for the user.
+		</p>
+		<div class="hz"></div>
+		<p>
+			<img src="_images/HZ_Interview-Tool_Loop_Ranking.png" alt="" /><br />
+			This is the tool interviewers use to organize their questions and input data into the system while speaking with the respondent. Again, the owner demands a lot 
+			of information to be available on screen. This proved to be challenging when trying to design for a potential lowest screen resolution of 1024x768
+		</p>
+		<div class="hz"></div>
+		<p>
+			<img src="_images/HZ_Profile-Editor_View_Recording.png" alt="" /><br />
+			This module is used to edit an interview's responses and prepare it for publication.
+		</p>
+		<div class="hz"></div>
+		<p>
+			<img src="_images/HZ_Overview-Editor_Action-Item.png" alt="" /><br />
+			This module is used to allow an analyst to build a report based on the respondent's feedback
+		</p>
+		<div class="hz"></div>
+		<p>
+			<img src="_images/HZ_Stakeholders_Add.png" alt="" /><br />
+			This screen is used to add "stakedholers" to a program. These are people who need to insert their directions to the Program Consultant to get the information
+			that will satisfy their research needs.
+		</p>
+	</div>
+</body>
+</html>
